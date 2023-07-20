84cdf8286d56ba3da545165540ebae7573cfbe18
Liene <liene.krista@gmail.com>
2023-07-21 02:56:20 +0300

    Added rxswift package. Created the base of the app.

---
+++ GIF Catalogue Application/GIF Catalogue Application/Controllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GIF Catalogue Application
+//
+//  Created by liene.krista.neimane on 20/07/2023.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var gifCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // eD2EKarMyNu9z4nBQaKzAC2Zyfg72oky
+
+}
+
